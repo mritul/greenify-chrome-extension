dc3f8c5f8b8ffa0557cec156d5d833f98b83341c
mani1911 <mail2aj19@gmail.com>
2023-02-12 00:55:56 +0530

    feat(App): Adds Login, Session Storage

--- popup/popup.js
+++ popup/popup.js
@@ -1,17 +1,47 @@
-console.log("Works");
+chrome.storage.sync.get(["access_token"], function (result) {
+  if (result.access_token) {
+    mainPage.style.display = "flex";
+    loginPage.style.display = "none";
+  }
+});
+
+const BACKEND_URL = "http://localhost:3000/auth/login";
 const continueBtn = document.querySelector(".btn");
 const username = document.querySelector("#username");
 const password = document.querySelector("#password");
 const mainPage = document.querySelector(".main");
 const loginPage = document.querySelector(".login");
+const route_to_login = document.querySelector("#loginPage");
+const invalid = document.querySelector(".invalid");
 
-continueBtn.addEventListener("click", (e) => {
-  console.log(username.value, password.value);
-  if (
-    username.value.toString() == username.value &&
-    password.value.toString() == password.value
-  ) {
+route_to_login.addEventListener("click", () => {
+  chrome.tabs.create({ url: "http://localhost:5173/" });
+});
+
+continueBtn.addEventListener("click", async (e) => {
+  const creds = { username: username.value, password: password.value };
+
+  try {
+    const res = await fetch(BACKEND_URL, {
+      method: "POST",
+      body: JSON.stringify(creds),
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+    });
+
+    const data = await res.json();
+    console.log(data);
+    if (data.status !== 200) {
+      invalid.style.display = "block";
+      return;
+    }
+
+    chrome.storage.sync.set({ access_token: [data.token] }, function (result) {
+      //func
+    });
+
     mainPage.style.display = "flex";
     loginPage.style.display = "none";
+  } catch (e) {
+    console.log(e);
   }
 });
