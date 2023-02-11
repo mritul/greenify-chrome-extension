fda1a055422eb7231f4c6b201170a7c479bc6cb5
mani1911 <mail2aj19@gmail.com>
2023-02-11 14:07:42 +0530

    feat(App): size for requests

--- background.js
+++ background.js
@@ -1,10 +1,56 @@
-chrome.webRequest.onSendHeaders.addListener(
+// let responseSize = 0;
+
+// //Request Header Size
+// chrome.webRequest.onSendHeaders.addListener(
+//   function (details) {
+//     console.log(
+//       "Request Header size:",
+//       details.requestHeaders.map((header) => header.value).join("").length
+//     );
+//   },
+//   { urls: ["<all_urls>"] },
+//   ["requestHeaders"]
+// );
+
+// // Response Header Size
+// chrome.webRequest.onHeadersReceived.addListener(
+//   function (details) {
+//     console.log(
+//       "Response Header size:",
+//       details.responseHeaders.map((header) => header.value).join("").length
+//     );
+//   },
+//   { urls: ["<all_urls>"] },
+//   ["responseHeaders"]
+// );
+
+let requestSize = 0;
+let responseSize = 0;
+
+urlFilter = "<all_urls>";
+
+chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
-    console.log(
-      "Request size:",
-      details.requestHeaders.map((header) => header.value).join("").length
-    );
+    console.log(details);
+    if (details.requestBody) {
+      requestSize += details.requestBody.raw
+        .map((chunk) => chunk.bytes.byteLength)
+        .reduce((sum, length) => sum + length, 0);
+      console.log("Request size:", requestSize);
+    }
   },
-  { urls: ["<all_urls>"] },
-  ["requestHeaders"]
+  { urls: [urlFilter] },
+  ["extraHeaders", "requestBody"]
 );
+
+// chrome.webRequest.onCompleted.addListener(
+//   function (details) {
+//     responseSize += details.responseHeaders
+//       .map((header) => header.value)
+//       .join("").length;
+//     responseSize += details.bodySize;
+//     console.log("Response size:", responseSize);
+//   },
+//   { urls: [urlFilter] },
+//   ["responseHeaders"]
+// );
