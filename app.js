fa290de01167400e9a8ab652c72b7647cc9fca88
mani1911 <mail2aj19@gmail.com>
2023-02-11 12:35:59 +0530

    feat(App): init

--- app.js
+++
@@ -1,5 +0,0 @@
-const btn = document.querySelector(".btn")
-console.log(btn);
-btn.addEventListener("click",()=>{
-    console.log("hi")
-})
--- background.js
+++ background.js
@@ -1,1 +1,10 @@
-console.log()
+chrome.webRequest.onSendHeaders.addListener(
+  function (details) {
+    console.log(
+      "Request size:",
+      details.requestHeaders.map((header) => header.value).join("").length
+    );
+  },
+  { urls: ["<all_urls>"] },
+  ["requestHeaders"]
+);
